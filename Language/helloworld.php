768260702cbbbdc642bbacca125358d530a08d26
Sonjoaquin <39743051+Sonjoaquin@users.noreply.github.com>
2019-10-08 11:30:13 +0200

    Create helloworld.php
    HelloWorld php english and spanish

---
+++ Language/helloworld.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8" />
+        <title> HelloWorld english and spanish</title>
+    </head>
+    <body>
+        <?php
+            echo 'HelloWorld';
+			echo 'HolaMundo';
+        ?>
+    </body>
+</html>
